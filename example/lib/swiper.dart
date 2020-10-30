59c5b4d66f05b78e82ec5102839dedf04a46bb50
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-30 18:05:32 +0530

    Update swiper.dart

--- example/lib/swiper.dart
+++ example/lib/swiper.dart
@@ -12,21 +12,26 @@ class VideoSwiper extends StatefulWidget {
 }
 
 class _VideoSwiperState extends State<VideoSwiper> {
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-    body:  new Swiper(
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
-      ),
-    );
+        appBar: new AppBar(
+          title: new Text("Swiper"),
+        ),
+        body: new Padding(
+            padding: EdgeInsets.fromLTRB(0, 25, 0, 25),
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return new Image.network(
+                  "http://via.placeholder.com/288x188",
+                  fit: BoxFit.fill,
+                );
+              },
+              itemCount: 10,
+              viewportFraction: 0.8,
+              scale: 0.9,
+              pagination: new SwiperPagination(builder: SwiperPagination.fraction),
+              control: new SwiperControl(),
+            )));
   }
 }
