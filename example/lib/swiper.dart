39db4b9a66cd9c28195dc111f662f887f7d59b8f
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-30 18:11:39 +0530

    Replace Image with Video Player

--- example/lib/swiper.dart
+++ example/lib/swiper.dart
@@ -1,3 +1,4 @@
+import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
@@ -22,16 +23,18 @@ class _VideoSwiperState extends State<VideoSwiper> {
             padding: EdgeInsets.fromLTRB(0, 25, 0, 25),
             child: Swiper(
               itemBuilder: (BuildContext context, int index) {
-                return new Image.network(
-                  "http://via.placeholder.com/288x188",
-                  fit: BoxFit.fill,
-                );
+                return new 
+                // Image.network(
+                //   "http://via.placeholder.com/288x188",
+                //   fit: BoxFit.fill,
+                // );
+                BetterPlayer.network("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4");
               },
               itemCount: 10,
               viewportFraction: 0.8,
               scale: 0.9,
-              pagination: new SwiperPagination(builder: SwiperPagination.fraction),
-              control: new SwiperControl(),
+              pagination: new SwiperPagination(builder: SwiperPagination.fraction, margin: EdgeInsets.fromLTRB(0, 0, 0, 0)),
+              control: new SwiperControl(iconNext: Icons.navigate_next_rounded, iconPrevious: Icons.navigate_before_rounded, size: 125),
             )));
   }
 }
